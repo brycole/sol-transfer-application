1468b2a0d4bf23f459996292cb73da2d7ba30b99
Bryan Cole <brycole@gmail.com>
2023-01-10 18:57:26 +0000

    add transfer function

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,19 @@ import dotenv from 'dotenv';
 import { getMinimumBalanceForRentExemptMultisig } from '@solana/spl-token';
 dotenv.config();
 
-const PROGRAM_ID = new Web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa")
-const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod")
+const PROGRAM_ID = new Web3.PublicKey("11111111111111111111111111111111")
 
 async function main() {
-    const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
-    const senderKey = JSON.parse(process.env.SENDER_KEY ?? '') as number[];
-    const receiverKey = JSON.parse(process.env.RECEIVER_KEY ?? '') as number[];
-    const sender = await initializeKeypair(connection, senderKey);
-    const receiver = await initializeKeypair(connection, receiverKey);
+    const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+    const senderKey = JSON.parse(process.env.SENDER_KEY ?? '') as number[]
+    const receiverKey = JSON.parse(process.env.RECEIVER_KEY ?? '') as number[]
+    const sender = await initializeKeypair(connection, senderKey)
+    const receiver = await initializeKeypair(connection, receiverKey)
   
-    console.log("Public key of Sender:", sender.publicKey.toBase58());
-    console.log("Public key of Receiver:", receiver .publicKey.toBase58());
+    console.log("Public key of Sender:", sender.publicKey.toBase58())
+    console.log("Public key of Receiver:", receiver .publicKey.toBase58())
+
+    await sendSol(connection, 0.1*Web3.LAMPORTS_PER_SOL, receiver, sender)
 }
 
 
@@ -27,6 +28,26 @@ async function initializeKeypair(
     const secretKey = Uint8Array.from(keypair);
     const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
     return keypairFromSecret;
+  }
+
+  async function sendSol(connection: Web3.Connection, amount: number, receiver: Web3.Keypair, sender: Web3.Keypair) {
+    const transaction = new Web3.Transaction()
+
+    const sendSolInstruction = Web3.SystemProgram.transfer(
+        {
+            fromPubkey: sender.publicKey,
+            toPubkey: receiver.publicKey, 
+            lamports: amount,
+        }
+    )
+
+    transaction.add(sendSolInstruction)
+
+    const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [sender])
+  
+    console.log(
+      `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+      )
   }
 
 main()
