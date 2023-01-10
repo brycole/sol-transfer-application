929ab72ef8f38576746e440a6c2c0c6850aa5a88
Bryan Cole <brycole@gmail.com>
2023-01-10 18:37:30 +0000

    fixes to initialize keypair

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as Web3 from '@solana/web3.js';
 import * as fs from 'fs';
 import dotenv from 'dotenv';
+import { getMinimumBalanceForRentExemptMultisig } from '@solana/spl-token';
 dotenv.config();
 
 const PROGRAM_ID = new Web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa")
@@ -8,8 +9,10 @@ const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eA
 
 async function main() {
     const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
-    const sender = await initializeKeypair(connection, "SENDER");
-    const receiver = await initializeKeypair(connection, "RECEIVER");
+    const senderKey = JSON.parse(process.env.SENDER_KEY ?? '') as number[];
+    const receiverKey = JSON.parse(process.env.RECEIVER_KEY ?? '') as number[];
+    const sender = await initializeKeypair(connection, senderKey);
+    const receiver = await initializeKeypair(connection, receiverKey);
   
     console.log("Public key of Sender:", sender.publicKey.toBase58());
     console.log("Public key of Receiver:", receiver .publicKey.toBase58());
@@ -18,17 +21,10 @@ async function main() {
 
 async function initializeKeypair(
     connection: Web3.Connection,
-    keypair: string
+    keypair: number[]
     ): Promise<Web3.Keypair> {
-   
-    if (keypair = "SENDER") { 
-        const secret = JSON.parse(process.env.SENDING_KEY ?? '') as number[]; 
-    }
-    else { 
-        const secret = JSON.parse(process.env.RECEIVING_KEY ?? '') as number[]; 
-    }
-    
-    const secretKey = Uint8Array.from(secret);
+       
+    const secretKey = Uint8Array.from(keypair);
     const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
     return keypairFromSecret;
   }
